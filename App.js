0d26638bc5501ae0239606af7860659b253abe88
DanielBCoffaro <33422680+DanielBCoffaro@users.noreply.github.com>
2017-11-06 10:36:06 -0500

    Initial push - no storage
    The app has a functional UI but does not save the note once it has been closed.

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Main from './app/components/Main';
+
+export default class TheApp extends React.Component {
+  render() {
+    return (
+        <Main/>
+    );
+  }
+}
+
